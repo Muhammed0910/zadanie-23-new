31a49d3b323a7c29bbdc315b0116e71e7f5aec12
Muhammed0910 <115030918+Muhammed0910@users.noreply.github.com>
2023-01-27 19:48:39 +0400

    Add files via upload

---
+++ zadanie 29/main.js
@@ -0,0 +1,19 @@
+1
+function add_ly(suffix, x, a, truler){
+    let suffix
+    let x
+    let truler
+    let a
+    if (a == 1){
+        truler = suffix + x
+    } else if (a == 0){
+        truler = false
+    }
+    return truler
+}
+console.log(add_ly(hopeless, ly, 1, true))
+2
+//?
+3
+//?
+4
